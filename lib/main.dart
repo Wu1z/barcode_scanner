100f640fe5e425c96b8e4881cafe1b3c52310dc8
Wu1z <wllmerscher@gmail.com>
2022-05-31 23:17:19 -0300

    Created the QrCode Page, inserting the barcodes in the firestore, creating the list of codes saved, creating the delete function and implementing the url_launcher to open url's

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,33 @@
+import 'package:barcode_reader/theme/theme.dart';
+import 'package:barcode_reader/view/qr_code_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'connection/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Barcode Scanner',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: lightColorScheme,
       ),
-      home: Container(),
+      home: const QrCodePage(),
     );
   }
 }
